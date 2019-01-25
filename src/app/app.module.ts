39029f6fdb697a31620b71ff3f9547109ce91382
Dash <youremail@domain.com>
2019-01-25 18:20:35 +0200

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { RootAdminComponent } from './components/root-admin/root-admin.component';
 import { HeaderComponent } from './components/header/header.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule, MatInputModule} from "@angular/material";
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -19,9 +24,18 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+      MatIconModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+    exports: [
+        MatFormFieldModule
+    ],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+    email = new FormControl('', [Validators.required, Validators.email]);
+    hide = true;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    getErrorMessage() {
+        return this.email.hasError('required') ? 'You must enter a value' :
+            this.email.hasError('email') ? 'Not a valid email' :
+                '';
+    }
 
 }
