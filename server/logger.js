7daa7f39eea825cb906bad5325d033342240cd7e
Piliponful <piliponful@gmail.com>
2019-02-12 21:11:08 +0200

    change where logs is stored

--- server/logger.js
+++ server/logger.js
@@ -1,5 +1,5 @@
 const path = require('path')
 const bunyan = require('bunyan')
-const log = bunyan.createLogger({ name: 'arialpoint-server', streams: [{ path: path.resolve(__dirname, 'log') }] })
+const log = bunyan.createLogger({ name: 'arialpoint-server', streams: [{ path: path.resolve(__dirname, 'logs/log') }] })
 
 module.exports = log
